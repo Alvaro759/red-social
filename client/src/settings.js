96e5d670ec4910ba5a7a0ffeba428ae8b43b8f67
Alvaro759 <acargar730@g.educaand.es>
2024-03-02 19:04:09 +0100

    Algo de relleno de ajustes

--- client/src/settings.js
+++ client/src/settings.js
@@ -1,10 +1,25 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState, useEffect } from "react";
 import Menu from "./Menu";
 import { DarkModeContext } from "./DarkModeContext";
 
 function Settings() {
   const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+  const [showEmail, setShowEmail] = useState(false);
+
+  useEffect(() => {
+    // Al cargar el componente, recuperamos el estado del checkbox del localStorage
+    const storedShowEmail = localStorage.getItem("showEmail");
+    if (storedShowEmail !== null) {
+      setShowEmail(storedShowEmail === "true");
+    }
+  }, []);
+
+  const handleEmailCheckboxChange = () => {
+    const newValue = !showEmail;
+    setShowEmail(newValue);
+    // Guardamos el estado del checkbox en el localStorage
+    localStorage.setItem("showEmail", newValue.toString());
+  };
 
   return (
     <div className={darkMode ? "main dark-mode" : "main"}>
@@ -23,6 +38,27 @@ function Settings() {
                 />
                 <span className="slider round"></span>
               </label>
+              <h3>Privacidad</h3>
+              <div className="form-select m-1">
+                <span>¿Quién puede enviarte mensajes?</span>
+                <select>
+                  <option selected>Todo el mundo</option>
+                  <option>Amigos y Amigos de Amigos</option>
+                  <option>Solo Amigos</option>
+                  <option>Nadie</option>
+                </select>
+              </div>
+              <div className="form-select m-1">
+                <span>Mostrar mi email</span>
+                <label className="switch">
+                  <input 
+                    type="checkbox" 
+                    checked={showEmail}
+                    onChange={handleEmailCheckboxChange}
+                  />
+                  <span className="slider round"></span>
+                </label>
+              </div>
             </div>
           </div>
         </div>
